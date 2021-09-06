ba3c3a000a8177084f4b1f0422e36f13dc14e5b5
Samuel Attard <samuel.r.attard@gmail.com>
2021-09-06 00:27:34 -0700

    Initial Commit

---
+++ AccountManager.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace ValorantAccountSwitcher
+{
+    class AccountManager
+    {
+        private static string RiotDataDirectory = Path.GetFullPath(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Riot Games", "Riot Client", "Data"));
+        private static string RiotSettingsFile = "RiotClientPrivateSettings.yaml";
+        private static string AccountDirectory = Path.GetFullPath(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Valorant Account Switcher"));
+
+        class RiotSettings
+        {
+            public PrivateSettings _private { get; set; }
+        }
+        class PrivateSettings
+        {
+            public RiotLogin riotLogin { get; set; }
+        }
+
+        class RiotLogin
+        {
+            public Persist persist { get; set; }
+        }
+
+        class Persist
+        {
+            public string region { get; set; }
+            public RiotSession session { get; set; }
+        }
+
+        class RiotSession
+        {
+            public List<RiotCookie> cookies { get; set; }
+        }
+
+        class RiotCookie
+        {
+            public string name { get; set; }
+            public string value { get; set; }
+        }
+
+        private static string GetUniqueIdentifierForCreds(string filename)
+        {
+            var contents = File.ReadAllText(filename);
+            var deserializer = new DeserializerBuilder().WithAttributeOverride<PrivateSettings>(
+                c => c.riotLogin,
+                new YamlMemberAttribute
+                {
+                    Alias = "riot-login"
+                }
+            ).IgnoreUnmatchedProperties().Build();
+            var settings = deserializer.Deserialize<RiotSettings>(new StringReader("_" + contents));
+            foreach (var cookie in settings._private.riotLogin.persist.session.cookies)
+            {
+                Console.WriteLine("Cookie: " + cookie.name);
+                if (cookie.name == "sub") return cookie.value;
+            }
+
+            return "Complete_Unknown";
+        }
+
+        public static List<string> GetAccountNames()
+        {
+            EnsureAccounts();
+            List<string> names = new List<string>();
+            foreach (string file in Directory.GetFiles(AccountDirectory))
+            {
+                if (Path.GetExtension(file) == ".yaml")
+                {
+                    names.Add(Path.GetFileNameWithoutExtension(file));
+                }
+            }
+            return names;
+        }
+
+        public static string GetAccountFilePath(string account)
+        {
+            EnsureAccounts();
+            return Path.Combine(AccountDirectory, account + ".yaml");
+        }
+
+        public static bool IsCurrentAccountUnknown()
+        {
+            if (!Directory.Exists(RiotDataDirectory)) return false;
+            string settingsPath = Path.Combine(RiotDataDirectory, RiotSettingsFile);
+            if (!File.Exists(settingsPath)) return false;
+
+            string currentId = GetUniqueIdentifierForCreds(settingsPath);
+            foreach (string account in GetAccountNames())
+            {
+                if (GetUniqueIdentifierForCreds(GetAccountFilePath(account)) == currentId) return false;
+            }
+
+            return true;
+        }
+
+        public static string GetCurrentAccountName()
+        {
+            if (!Directory.Exists(RiotDataDirectory)) return "";
+            string settingsPath = Path.Combine(RiotDataDirectory, RiotSettingsFile);
+            if (!File.Exists(settingsPath)) return "";
+
+            string currentId = GetUniqueIdentifierForCreds(settingsPath);
+            foreach (string account in GetAccountNames())
+            {
+                if (GetUniqueIdentifierForCreds(GetAccountFilePath(account)) == currentId) return account;
+            }
+
+            return "";
+        }
+
+        public static void CopyCurrentToAccount(string account)
+        {
+            EnsureAccounts();
+            if (!Directory.Exists(RiotDataDirectory)) return;
+            string settingsPath = Path.Combine(RiotDataDirectory, RiotSettingsFile);
+            if (!File.Exists(settingsPath)) return;
+            string accountPath = GetAccountFilePath(account);
+
+            if (File.Exists(accountPath))
+            {
+                File.Delete(accountPath);
+            }
+            File.Copy(settingsPath, GetAccountFilePath(account));
+        }
+
+        public static void CopyAccountToCurrent(string account)
+        {
+            EnsureAccounts();
+            if (!Directory.Exists(RiotDataDirectory)) return;
+            string settingsPath = Path.Combine(RiotDataDirectory, RiotSettingsFile);
+            if (!File.Exists(settingsPath)) return;
+            string accountPath = GetAccountFilePath(account);
+            if (!File.Exists(accountPath)) return;
+
+            File.Delete(settingsPath);
+            File.Copy(accountPath, settingsPath);
+        }
+
+        private static void EnsureAccounts()
+        {
+            if (!Directory.Exists(AccountDirectory))
+            {
+                Directory.CreateDirectory(AccountDirectory);
+            }
+        }
+    }
+}
---
+++ ValorantApplicationContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValorantAccountSwitcher
+{
+    class ValorantApplicationContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+
+        public ValorantApplicationContext()
+        {
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location),
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("-"),
+                    new MenuItem("Exit", Exit)
+                }),
+                Text = "Valorant Account Switcher",
+                Visible = true
+            };
+            PopulateAccountList();
+        }
+
+        void PopulateAccountList()
+        {
+            trayIcon.ContextMenu.MenuItems.Clear();
+            string currentAccount = AccountManager.GetCurrentAccountName();
+            foreach (string account in AccountManager.GetAccountNames())
+            {
+                var item = new MenuItem(account, ChangeAccount);
+                if (account == currentAccount)
+                {
+                    item.Checked = true;
+                }
+                trayIcon.ContextMenu.MenuItems.Add(item);
+            }
+            if (trayIcon.ContextMenu.MenuItems.Count > 0)
+            {
+                trayIcon.ContextMenu.MenuItems.Add(new MenuItem("-"));
+            }
+            if (AccountManager.IsCurrentAccountUnknown())
+            {
+                trayIcon.ContextMenu.MenuItems.Add(new MenuItem("Add current account...", AddCurrentAccount));
+            }
+            trayIcon.ContextMenu.MenuItems.Add(new MenuItem("Exit", Exit));
+        }
+
+        void ChangeAccount(object sender, EventArgs e)
+        {
+            MenuItem temp = sender as MenuItem;
+            string accountName = temp.Text;
+            AccountManager.CopyAccountToCurrent(accountName);
+            PopulateAccountList();
+        }
+
+        void AddCurrentAccount(object sender, EventArgs e)
+        {
+            string accountName = Microsoft.VisualBasic.Interaction.InputBox("Please enter an account name for this Valorant account",
+                       "New Account",
+                       "");
+            if (accountName.Length == 0) return;
+
+            Regex r = new Regex("^[a-zA-Z0-9#]*$");
+            if (!r.IsMatch(accountName))
+            {
+                Microsoft.VisualBasic.Interaction.MsgBox("Account names must consist of numbers and letters, no spaces or puncuation are allowed", Microsoft.VisualBasic.MsgBoxStyle.Exclamation, "Invalid Account Name");
+                return;
+            }
+
+            AccountManager.CopyCurrentToAccount(accountName);
+            PopulateAccountList();
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+
+            Application.Exit();
+        }
+    }
+}
